ba9cf12e81e9bd516e77e5850aefca196c3caaf2
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2018-11-04 01:20:21 -0500

    Text Editor Works

--- main.js
+++ main.js
@@ -1,57 +1,92 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
-const mainMenuTemplate = [
-  {
-    label: 'File',
-    submenu:[
-    {
-      label: "New Document"
-    }]
-  }
-];
-
-const {app, BrowserWindow, Menu} = require('electron');
-let mainWindow
-
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadFile('index.html')
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+if (require.main !== module) {
+  require('update-electron-app')({
+    logger: require('electron-log')
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+const path = require('path')
 
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  //insert Menu
-  Menu.setApplicationMenu(mainMenu);
-// Quit when all windows are closed.
+const {app, BrowserWindow} = require('electron')
 
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+const debug = /--debug/.test(process.argv[2])
+
+if (process.mas) app.setName('Electron APIs')
+
+let mainWindow = null
+
+function initialize () {
+  makeSingleInstance()
+
+  loadDemos()
+
+  function createWindow () {
+    const windowOptions = {
+      width: 1080,
+      minWidth: 680,
+      height: 840,
+      title: app.getName()
+    }
+
+    if (process.platform === 'linux') {
+      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+
+    // Launch fullscreen with DevTools open, usage: npm run debug
+    if (debug) {
+      mainWindow.webContents.openDevTools()
+      mainWindow.maximize()
+      require('devtron').install()
+    }
+
+    mainWindow.on('closed', () => {
+      mainWindow = null
+    })
   }
-})
 
-
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  app.on('ready', () => {
     createWindow()
-  }
-})
+  })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
+}
+
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return
+
+  app.requestSingleInstanceLock()
+
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+// Require each JS file in the main-process dir
+function loadDemos () {
+  require('./scripts/open.js')
+  require('./scripts/save.js')
+}
+
+initialize()
---
+++ renderer/save-file.js
@@ -0,0 +1,18 @@
+const {ipcRenderer} = require('electron')
+const fs = require('fs')
+const saveBtn = document.getElementById('save-button')
+
+saveBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('save-dialog')
+})
+
+ipcRenderer.on('saved-file', (event, path) => {
+	console.log(path)
+	var data = document.getElementById('selected-file').value
+	console.log(data)
+	fs.writeFile(path,data,function(err){
+	if (err)
+		throw err;
+	console.log("File was Saved")
+	})
+})
